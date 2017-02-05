8d74f08e56ea62bf6c01ec500e2dcaab3f0d91c4
Abdulrahman Sahmoud <ahsahmoud@ucdavis.edu>
2017-02-05 01:16:11 -0800

    initial commit

---
+++ CourseDetailViewController.swift
@@ -0,0 +1,42 @@
+//
+//  NoteDetailViewController.swift
+//  TMA
+//
+//  Created by Abdulrahman Sahmoud on 2/1/17.
+//  Copyright © 2017 Abdulrahman Sahmoud. All rights reserved.
+//
+
+import UIKit
+
+class NoteDetailViewController: UIViewController {
+
+    @IBOutlet weak var NoteContent: UILabel!
+    var course: Course!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        NoteContent.text = course.content
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ TMA/TMA/Course.swift
@@ -0,0 +1,14 @@
+//
+//  Course.swift
+//  TMA
+//
+//  Created by Abdulrahman Sahmoud on 2/1/17.
+//  Copyright © 2017 Abdulrahman Sahmoud. All rights reserved.
+//
+
+import Foundation
+
+class Course {
+    var title = ""
+    var content = ""
+}
