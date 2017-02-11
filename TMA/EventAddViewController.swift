6cfe9cb035d3774088ac6b8deaef3fefae455b88
asbasi <arvinder.singh.basi@gmail.com>
2017-02-10 20:27:37 -0800

    Implementing a basic PlannerTableViewController. Still need to add groupings and edit/add page

---
+++ TMA/EventAddViewController.swift
@@ -0,0 +1,35 @@
+//
+//  EventAddViewController.swift
+//  TMA
+//
+//  Created by Arvinder Basi on 2/10/17.
+//  Copyright © 2017 Abdulrahman Sahmoud. All rights reserved.
+//
+
+import UIKit
+
+class EventAddViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
