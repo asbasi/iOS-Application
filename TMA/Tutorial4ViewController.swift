d40ee7bcfaa368db74295b624e18f01768c7c809
mnjtan <mj_daydreamer@minjie-tans-mbp.gateway.pace.com>
2017-04-19 12:52:22 -0700

    Tutorial Add Back to Master

---
+++ TMA/Tutorial4ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  Tutorial4ViewController.swift
+//  TMA
+//
+//  Created by Minjie Tan on 4/19/17.
+//  Copyright © 2017 Abdulrahman Sahmoud. All rights reserved.
+//
+
+import UIKit
+
+class Tutorial4ViewController: UIViewController {
+
+    @IBOutlet weak var Photo: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.Photo.image = UIImage(named: "test")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ TMA/TutorialViewController.swift
@@ -0,0 +1,54 @@
+//
+//  TutorialViewController.swift
+//  TMA
+//
+//  Created by Minjie Tan on 4/19/17.
+//  Copyright © 2017 Abdulrahman Sahmoud. All rights reserved.
+//
+
+import UIKit
+
+class TutorialViewController: UIViewController {
+
+    @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var pageControl: UIPageControl!
+    
+    var tutorialPageViewController: TutorialPageViewController? {
+        didSet {
+            tutorialPageViewController?.tutorialDelegate = self
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        pageControl.addTarget(self, action: "didChangePageControlValue", for: .valueChanged)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let tutorialPageViewController = segue.destination as? TutorialPageViewController {
+            self.tutorialPageViewController = tutorialPageViewController
+        }
+    }
+    
+    /**
+     Fired when the user taps on the pageControl to change its current page.
+     */
+    func didChangePageControlValue() {
+        tutorialPageViewController?.scrollToViewController(index: pageControl.currentPage)
+    }
+}
+
+extension TutorialViewController: TutorialPageViewControllerDelegate {
+    
+    func tutorialPageViewController(_ tutorialPageViewController: TutorialPageViewController,
+                                    didUpdatePageCount count: Int) {
+        pageControl.numberOfPages = count
+    }
+    
+    func tutorialPageViewController(_ tutorialPageViewController: TutorialPageViewController,
+                                    didUpdatePageIndex index: Int) {
+        pageControl.currentPage = index
+    }
+    
+}
