16ba3585b344ca2a0d2c9c19b34fe6dc63c9ea07
Abdulrahman Sahmoud <ahsahmoud@ucdavis.edu>
2017-04-26 17:45:17 -0700

    uc davis login changes: disables done button when app is loading, disables done button when text fields are empty

--- LoginTableViewController.swift
+++ LoginTableViewController.swift
@@ -15,9 +15,24 @@ class LoginTableViewController: UITableViewController {
     let realm = try! Realm()
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var usernameTextField: UITextField!
+    var indicator = UIActivityIndicatorView()
+    
+    func activityIndicator() {
+        indicator = UIActivityIndicatorView(frame: CGRect(x: 0, y: 0,width: 40,height: 40))
+        indicator.activityIndicatorViewStyle = UIActivityIndicatorViewStyle.gray
+        indicator.center = self.view.center
+        self.view.addSubview(indicator)
+    }
     
     @IBAction func Done(_ sender: Any) {
         let currentQuarter = self.realm.objects(Quarter.self).filter("current = true").first
+        
+        self.dismissKeyboard()
+        self.navigationItem.rightBarButtonItem?.isEnabled = false;
+        
+        indicator.startAnimating()
+        indicator.backgroundColor = UIColor.white
+        
         
         Alamofire.request("http://sahmudi.com/?username=\(usernameTextField.text!)&password=\(passwordTextField.text!)", method: .get, encoding: JSONEncoding.default)
             .responseJSON { response in
@@ -42,24 +57,25 @@ class LoginTableViewController: UITableViewController {
                                 Helpers.DB_insert(obj: course)
                             } // end for crn
                             
-                        } //end DispathQueue.main
+                            self.navigationItem.rightBarButtonItem?.isEnabled = true;
+                            self.indicator.stopAnimating()
+                            self.indicator.hidesWhenStopped = true
                     
-                        if Array(responseDict.keys).count == 0 {
-                            let alert = UIAlertController(title: "Incorrect Credentials", message: "Incorrect username or password.", preferredStyle: UIAlertControllerStyle.alert)
-                            alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.default, handler: nil))
-                            self.present(alert, animated: true, completion: nil)
-                        }
-                        else {
-                            DispatchQueue.main.async {
+                            if Array(responseDict.keys).count == 0 {
+                                let alert = UIAlertController(title: "Incorrect Credentials", message: "Incorrect username or password.", preferredStyle: UIAlertControllerStyle.alert)
+                                alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.default, handler: nil))
+                                self.present(alert, animated: true, completion: nil)
+                            }
+                            else {
                                 let storage = Storage()
                                 storage.value = "imported_courses"
                                 Helpers.DB_insert(obj: storage)
-                            }
-                            let alert = UIAlertController(title: "Success", message: "Courses imported correctly", preferredStyle: UIAlertControllerStyle.alert)
-                            alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.default, handler: { action in
+                                let alert = UIAlertController(title: "Success", message: "Courses imported correctly", preferredStyle: UIAlertControllerStyle.alert)
+                                alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.default, handler: { action in
                                 self.dismiss(animated: true, completion: nil)
-                            }))
-                            self.present(alert, animated: true, completion: nil)
+                                }))
+                                self.present(alert, animated: true, completion: nil)
+                            }
                         }
                         
                         print("-------------------------------------------")
@@ -80,9 +96,20 @@ class LoginTableViewController: UITableViewController {
     @IBAction func Cancel(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
     }
+    @IBAction func usernameTextFieldChanged(_ sender: Any) {
+        checkAllTextFields()
+    }
+    
+    @IBAction func passwordTextFieldChanged(_ sender: Any) {
+        checkAllTextFields()
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.navigationItem.rightBarButtonItem?.isEnabled = false;
+        
+        
         let currentQuarters = self.realm.objects(Quarter.self).filter("current = true")
         if currentQuarters.count != 1 {
             let alert = UIAlertController(title: "Current Quarter Error", message: "You must have one current quarter before you can create events.", preferredStyle: UIAlertControllerStyle.alert)
@@ -103,7 +130,19 @@ class LoginTableViewController: UITableViewController {
         }
         
     }
-
+    
+    
+    
+    func checkAllTextFields() {
+        if ((usernameTextField.text?.isEmpty)! || (passwordTextField.text?.isEmpty)!) {
+            self.navigationItem.rightBarButtonItem?.isEnabled = false;
+        }
+        else {
+            self.navigationItem.rightBarButtonItem?.isEnabled = true;
+        }
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
