362570631f69ea5ab7c036710ed63c9e446453b7
mnjtan <mnjtan@ucdavis.edu>
2017-05-24 16:01:31 -0700

    acknowledgements

--- TMA/AcknowledgementsTableViewController.swift
+++ TMA/AcknowledgementsTableViewController.swift
@@ -7,89 +7,48 @@
 //
 
 import UIKit
+import SafariServices
 
 class AcknowledgementsTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.section == 0 && indexPath.row == 0 {
+            let svc = SFSafariViewController(url: URL(string:"https://github.com/Alamofire/Alamofire")!)
+            present(svc, animated: true, completion: nil)
+        }
+        else if indexPath.section == 1 && indexPath.row == 0 {
+            let svc = SFSafariViewController(url: URL(string:"https://github.com/WenchaoD/FSCalendar")!)
+            present(svc, animated: true, completion: nil)
+        }
+        else if indexPath.section == 2 && indexPath.row == 0 {
+            let svc = SFSafariViewController(url: URL(string:"https://github.com/Boris-Em/BEMCheckBox")!)
+            present(svc, animated: true, completion: nil)
+        }
+        else if indexPath.section == 3 && indexPath.row == 0 {
+            let svc = SFSafariViewController(url: URL(string:"https://github.com/danielgindi/Charts")!)
+            present(svc, animated: true, completion: nil)
+        }
+        else if indexPath.section == 4 && indexPath.row == 0 {
+            let svc = SFSafariViewController(url: URL(string:"https://github.com/i-schuetz/PieCharts")!)
+            present(svc, animated: true, completion: nil)
+        }
+        else if indexPath.section == 5 && indexPath.row == 0 {
+            let svc = SFSafariViewController(url: URL(string:"https://github.com/PureLayout/PureLayout")!)
+            present(svc, animated: true, completion: nil)
+        }
+        else if indexPath.section == 6 && indexPath.row == 0 {
+            let svc = SFSafariViewController(url: URL(string:"https://github.com/realm")!)
+            present(svc, animated: true, completion: nil)
+        }
+        else if indexPath.section == 7 && indexPath.row == 0 {
+            let svc = SFSafariViewController(url: URL(string:"https://realm.io/docs/swift/2.6.2/")!)
+            present(svc, animated: true, completion: nil)
+        }
+        
     }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
